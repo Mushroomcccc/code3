af6e47ddac9bb14e8f653aecfefa93a19340877b
1163710212 <260813501@qq.com>
2025-10-22 21:58:33 +0800

    second commit

--- run_llama/run_llama1.py
+++ run_llama/run_llama1.py
@@ -1,7 +1,7 @@
 import os
 import asyncio
 
-# 设置运行端口
+
 os.environ.update({'OLLAMA_HOST': '0.0.0.0:6011'})
 
 async def run_process(cmd, verb=True, path=None):
@@ -27,13 +27,11 @@ async def run_process(cmd, verb=True, path=None):
         await asyncio.gather(pipe(p.stdout))
 
 
-# ✅ 把异步代码包装在主函数中
 async def main():
     await asyncio.gather(
         run_process(['ollama', 'serve']),
     )
 
 
-# ✅ 使用 asyncio.run() 启动主协程
 if __name__ == "__main__":
     asyncio.run(main())
